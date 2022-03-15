c302073ee9ef4baab82f745417bde5e37e3593a4
BorisGitSaf <75093810+BorisGitSaf@users.noreply.github.com>
2022-03-15 16:24:24 +0300

    Add files via upload
    ДЗ 2

---
+++ flita2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	FILE *S1, *S2;
+	int N = 0;
+	char c[1024];
+
+	for (int i = 0; i < 1024; i ++) {
+		c[i] = '/0';
+	}
+	S1 = fopen(argv[1], "r");
+	fgets(c, 1024, S1);
+	for (int i = 0; i < 1024; i ++) {
+		if (c[i] == ' ')
+			N++;
+	}
+	fclose(S1);
+	S1 = fopen(argv[1], "r");
+	S2 = fopen(argv[2], "w");
+	fprintf(S2, "graph G {\n");
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			fscanf(S1, "%s ", &c);
+			if (c[0] != '0' && j >= i)
+				fprintf(S2, "\t%c -- %c [label = %s];\n", 'A' + i, 'A' + j, c);
+		}		
+	}
+	fprintf(S2, "}");
+	fclose(S1);
+	fclose(S2);
+	return 0;
+}
