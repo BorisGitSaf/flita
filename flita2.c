6b7f88aa631844bbb1a47f78aa7a59f1ab43d77e
BorisGitSaf <75093810+BorisGitSaf@users.noreply.github.com>
2022-03-22 14:45:31 +0300

    Update flita2.c
    Автоматический вывод изображений и учет вершин неинцендентных ни одной другой

--- flita2.c
+++ flita2.c
@@ -3,35 +3,44 @@
 
 
 int main(int argc, char *argv[]) {
-        FILE *S1, *S2;
-        int N = 0;
-        char c[1024];
+	FILE *S1, *S2;
+	int N = 0;
+	short f;
+	char c[1024];
 
-        for (int i = 0; i < 1024; i ++) {
-                c[i] = '/0';
-        }
-        S1 = fopen(argv[1], "r");
-        fgets(c, 1024, S1);
-        for (int i = 0; i < 1024; i ++) {
-                if (c[i] == ' ')
-                        N++;
-        }
-        fclose(S1);
-        S1 = fopen(argv[1], "r");
-        S2 = fopen(argv[2], "w");
-        fprintf(S2, "graph G {\n");
+	for (int i = 0; i < 1024; i ++) {
+		c[i] = '/0';
+	}
+	S1 = fopen(argv[1], "r");
+	fgets(c, 1024, S1);
+	for (int i = 0; i < 1024; i ++) {
+		if (c[i] == ' ')
+			N++;
+	}
+	fclose(S1);
+	S1 = fopen(argv[1], "r");
+	S2 = fopen(argv[2], "w");
+	fprintf(S2, "graph G {\n");
 
-        for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                        fscanf(S1, "%s ", &c);
-                        if (c[0] != '0' && j >= i)
-                                fprintf(S2, "\t%c -- %c [label = %s];\n", 'A' + i, 'A' + j, c);
-                }
-        }
-        fprintf(S2, "}");
-        fclose(S1);
-        fclose(S2);
-        sprintf(c, "dot %s -Tpng -o image.png", argv[2]);
-        int status = system(c);
-        return 0;
+	for (int i = 0; i < N; i++) {
+		f = 0;
+		for (int j = 0; j < N; j++) {
+			fscanf(S1, "%s ", &c);
+			if (c[0] != '0')
+				f = 1;
+			if (c[0] != '0' && j >= i)
+				fprintf(S2, "\t%c -- %c [label = %s];\n", 'A' + i, 'A' + j, c);
+		}
+		if (!f)
+			fprintf(S2, "\t%c;\n", 'A' + i);
+					
+	}
+	fprintf(S2, "}");
+	fclose(S1);
+	fclose(S2);
+	sprintf(c, "dot %s -Tpng -o image.png", argv[2]);
+	int status = system(c);
+	sprintf(c, "xdg-open image.png");
+	status = system(c);
+	return 0;
 }
